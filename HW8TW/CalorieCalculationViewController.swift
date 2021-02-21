4f8d96113bafd2499f98d19d7515aa36881a3b51
vladislav2 <etter.id75@gmail.com>
2021-02-21 22:38:43 +0200

    Setting textFields

--- HW8TW/CalorieCalculationViewController.swift
+++ HW8TW/CalorieCalculationViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class CalorieCalculationViewController: UIViewController {
 
+  @IBOutlet weak var genderSegmentedControl: UISegmentedControl!
+  
   @IBOutlet weak var heightTextField: UITextField!
   @IBOutlet weak var weightTextField: UITextField!
   @IBOutlet weak var ageTextField: UITextField!
@@ -17,10 +19,14 @@ class CalorieCalculationViewController: UIViewController {
   
   @IBOutlet weak var resultLabel: UILabel!
   
+  private var activity = 1.2
+  private var result = 0
+  
   override func viewDidLoad() {
-        super.viewDidLoad()
-
-    }
+    super.viewDidLoad()
+    addDoneButton(to: heightTextField, weightTextField, ageTextField)
+    
+  }
     
   @IBAction func changeGender(_ sender: UISegmentedControl) {
     
@@ -32,7 +38,26 @@ class CalorieCalculationViewController: UIViewController {
   
   @IBAction func calculateButtonPressed() {
     
+    if genderSegmentedControl.selectedSegmentIndex == 0 {
+      let height = Double(heightTextField.text ?? "1") ?? 1
+      let weight = Double(weightTextField.text ?? "1") ?? 1
+      let age = Double(ageTextField.text ?? "1") ?? 1
+      let resultMale = 88.36 + (4.8 * height) + (13.4 * weight) - (5.7 * age)
+      result = Int(resultMale * activity)
+      resultLabel.text = String(result)
+    } else {
+      let height = Double(heightTextField.text ?? "1") ?? 1
+      let weight = Double(weightTextField.text ?? "1") ?? 1
+      let age = Double(ageTextField.text ?? "1") ?? 1
+      let resultFemale = 447.6 + (3.1 * height) + (9.2 * weight) - (4.3 * age)
+      result = Int(resultFemale * activity)
+      resultLabel.text = String(result)
+    }
+    
   }
+  
+//  для мужчин: BMR = 88.36 + (13.4 x вес, кг) + (4.8 х рост, см) – (5.7 х возраст, лет)
+//  для женщин: BMR = 447.6 + (9.2 x вес, кг) + (3.1 х рост, cм) – (4.3 х возраст, лет)
   
     /*
     // MARK: - Navigation
@@ -43,5 +68,55 @@ class CalorieCalculationViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+  
+}
 
+// MARK: - Alert Controller
+
+extension CalorieCalculationViewController {
+    private func showAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default) { _ in
+            
+        }
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
+}
+
+// MARK: - Work with keyboard
+
+extension CalorieCalculationViewController: UITextFieldDelegate {
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == ageTextField {
+          //passwordTextField.becomeFirstResponder()
+        } else {
+          //logInPressed()
+        }
+        return true
+    }
+}
+
+extension CalorieCalculationViewController {
+  private func addDoneButton(to textFields: UITextField...) {
+    textFields.forEach { (textField) in
+      let keyboardToolBar = UIToolbar()
+      textField.inputAccessoryView = keyboardToolBar
+      keyboardToolBar.sizeToFit()
+      
+      let doneButton = UIBarButtonItem(title: "Done", style: .done, target: self, action: #selector(didTapButton))
+      
+      let flexBarButton = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+      keyboardToolBar.items = [flexBarButton, doneButton]
+    }
+  }
+  
+  @objc private func didTapButton() {
+    view.endEditing(true)
+  }
 }
---
+++ HW8TW/Enums.swift
@@ -0,0 +1,31 @@
+//
+//  Enums.swift
+//  HW8TW
+//
+//  Created by user on 21.02.2021.
+//
+
+import Foundation
+
+enum Activity: Double {
+  case minimal = 1.2
+  case low = 1.375
+  case middle = 1.55
+  case high = 1.725
+  case overHigh = 1.9
+  
+  var valueForActivity: String {
+    switch self {
+    case .minimal:
+      return "Минимальная"
+    case .low:
+      return "Низкая"
+    case .middle:
+      return "Средняя"
+    case .high:
+      return "Высокая"
+    case .overHigh:
+      return "Очень высокая"
+    }
+  }
+}
