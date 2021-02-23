80f26d5d557e35c606df8e646a75c2d50fd55969
Dmitry Pavlov <perr07@yandex.ru>
2021-02-23 13:20:27 +0300

    Work launchScreen

--- HW8TW/ViewControllers/ViewController.swift
+++ HW8TW/ViewControllers/ViewController.swift
@@ -9,12 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var imtOutlet: UIButton!
     @IBOutlet weak var kkalOutlet: UIButton!
     
     override func viewDidLoad() {
     super.viewDidLoad()
-        
+    
+        nameLabel.layer.masksToBounds = true
+    nameLabel.layer.cornerRadius = 10
     imtOutlet.layer.cornerRadius = 50
     kkalOutlet.layer.cornerRadius = 50
     
